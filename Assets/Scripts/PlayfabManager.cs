60297fae0ed10b8c280d43dc6852b99f16435727
Vladimir Shatunov <vovahat00@gmail.com>
2022-01-07 23:21:55 +0500

    + DOTween pack + leaderboard

---
+++ Assets/Scripts/PlayfabManager.cs
@@ -0,0 +1,94 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using PlayFab;
+using PlayFab.ClientModels;
+using UnityEngine.Events;
+
+public class PlayfabManager : MonoBehaviour
+{
+    public UnityEvent OnSuccessLogin;
+    public GameObject rowPrefab;
+    public Transform rowsParent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Login();
+    }
+
+    void Login()
+    {
+        var request = new LoginWithCustomIDRequest
+        {
+            CustomId = SystemInfo.deviceUniqueIdentifier,
+            CreateAccount = true
+        };
+        PlayFabClientAPI.LoginWithCustomID(request, OnSuccess, OnError);
+    }
+
+    void OnSuccess(LoginResult result)
+    {
+        Debug.Log("Successful login/account create!");
+        OnSuccessLogin?.Invoke();
+    }
+
+    void OnError(PlayFabError error)
+    {
+        Debug.Log("Error while logging in/creating account!");
+        Debug.Log(error.GenerateErrorReport());
+    }
+
+    public void SendLeaderboard(int score)
+    {
+        var request = new UpdatePlayerStatisticsRequest
+        {
+            Statistics = new List<StatisticUpdate>
+            {
+                new StatisticUpdate
+                {
+                    StatisticName = "OTARA-Scores",
+                    Value = score
+                }
+            }
+        };
+
+        PlayFabClientAPI.UpdatePlayerStatistics(request, OnLeaderboardUpdate, OnError);
+    }
+
+    void OnLeaderboardUpdate(UpdatePlayerStatisticsResult result)
+    {
+        Debug.Log("Successful leaderboard sent");
+    }
+
+    public void GetLeaderboard()
+    {
+        var request = new GetLeaderboardRequest
+        {
+            StatisticName = "OTARA-Scores",
+            StartPosition = 0,
+            MaxResultsCount = 10
+        };
+
+        PlayFabClientAPI.GetLeaderboard(request, OnLeaderboardGet, OnError);
+    }
+    void OnLeaderboardGet(GetLeaderboardResult result)
+    {
+        foreach(Transform item in rowsParent)
+        {
+            Destroy(item.gameObject);
+        }
+
+        foreach (var item in result.Leaderboard)
+        {
+            GameObject newGO = Instantiate(rowPrefab, rowsParent);
+            TextMeshProUGUI[] texts = newGO.GetComponentsInChildren<TextMeshProUGUI>();
+            texts[0].text = (item.Position + 1).ToString();
+            texts[1].text = item.PlayFabId.ToString();
+            texts[2].text = item.StatValue.ToString();
+
+            Debug.Log(item.Position + " " + item.PlayFabId + " " + item.StatValue);
+        }
+    }
+}
